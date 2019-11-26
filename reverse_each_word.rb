83274840475cd82cd1ba99b491faaadc582e7355
brandon moro <brandon_078@hotmail.com>
2019-11-26 03:08:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(string)
   string.each do |quote|
-    quote.reverse
+    string_reversal = []
+    string_reversal << quote
+    string_reversal.reverse
   end
+  string_reversal
 end
 
   
